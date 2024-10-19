e6a57f8f0c9af13b18e7a718e9279db5060e1509
Cauan <cauanvictor0325@gmail.com>
2024-10-19 20:17:25 -0300

    update: Alterando nome dos produtos

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,20 +3,20 @@ import './Products.css';
 
 const Products: React.FC = () => {
   const products = [
-    { id: 1, name: 'Caderno Fofinho', image: '/images/produto-1.jpg' },
-    { id: 2, name: 'Caneta Colorida', image: '/images/produto-2.jpg' },
-    { id: 3, name: 'Adesivos Divertidos', image: '/images/produto-3.jpg' },
-    { id: 4, name: 'Post-it Amoroso', image: '/images/produto-4.jpg' },
-    { id: 5, name: 'Mochila Linda', image: '/images/produto-5.jpg' },
-    { id: 6, name: 'Estojo Estiloso', image: '/images/produto-6.jpg' },
-    { id: 7, name: 'Lápis de Cor', image: '/images/produto-7.jpg' },
-    { id: 8, name: 'Copo Térmico', image: '/images/produto-8.jpg' },
-    { id: 9, name: 'Régua Criativa', image: '/images/produto-9.jpg' },
-    { id: 10, name: 'Canecas Fofas', image: '/images/produto-10.jpg' },
-    { id: 11, name: 'Caderno de Anotações', image: '/images/produto-11.jpg' },
+    { id: 1, name: 'Estojo Fofinho', image: '/images/produto-1.jpg' },
+    { id: 2, name: 'Livro Bege Fofo', image: '/images/produto-2.jpg' },
+    { id: 3, name: 'Canetas Fofas', image: '/images/produto-3.jpg' },
+    { id: 4, name: 'Lapiseiras Fofinhas', image: '/images/produto-4.jpg' },
+    { id: 5, name: 'Livro Azul Fofo', image: '/images/produto-5.jpg' },
+    { id: 6, name: 'Canetas Fofas', image: '/images/produto-6.jpg' },
+    { id: 7, name: 'Livro Verde Fofo', image: '/images/produto-7.jpg' },
+    { id: 8, name: 'Livro Rosa Fofo', image: '/images/produto-8.jpg' },
+    { id: 9, name: 'Borrachas Fofas', image: '/images/produto-9.jpg' },
+    { id: 10, name: 'Livros Fofos', image: '/images/produto-10.jpg' },
+    { id: 11, name: 'Canetas Fofas', image: '/images/produto-11.jpg' },
   ];
 
-  const handleProductClick = (productName: string) => {  // Definição do tipo
+  const handleProductClick = (productName: string) => {
     const message = `Oi! Quero comprar ${productName}`;
     const whatsappLink = `https://api.whatsapp.com/send?text=${encodeURIComponent(message)}`;
     window.open(whatsappLink, '_blank');
